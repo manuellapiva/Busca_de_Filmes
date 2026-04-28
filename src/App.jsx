4e1e764b33eca06d0d63f4020c84a7ae61d2e87a
manuellapiva <manuellabpiva@gmail.com>
2026-04-28 07:46:55 -0300

    constantes e alteração de efeito

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css'
+import { useState, useEffect } from 'react';
 
 function App () {
+  const [filme, setFilme] = useState('');
+  const [lista, setLista] = useState([]);
+  const [detalhes, setDetalhes] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [erro, setErro] = useState(null);
+  const [temporada, setTemporada] = useState(1);
+
+  useEffect(() => {
+    const ultimo = localStorage.getItem('ultimaBusca');
+    if (ultimo) setFilme(ultimo);
+  }, []);
+
   return (
     <>
     
